171db6bc46ed94885ef30c145e5e143e511dd5b8
Vitalie Rosescu <vitalier@hotmail.be>
2025-01-29 17:09:28 +0100

    yo

--- src/js/flipCode.js
+++ src/js/flipCode.js
@@ -16,83 +16,86 @@ function flipCode() {
     if (!isNaN(attrVal) && defaultValType === 'number') return +attrVal
     return defaultVal
   }
-
-  console.log('work')
-  ScrollTrigger.normalizeScroll(true)
-  // scrollflip component
-  $("[tr-scrollflip-element='component']").each(function (index) {
-    let componentEl = $(this),
-      originEl = componentEl.find("[tr-scrollflip-element='origin']"),
-      targetEl = componentEl.find("[tr-scrollflip-element='target']"),
-      scrubStartEl = componentEl.find('[tr-scrollflip-scrubstart]'),
-      scrubEndEl = componentEl.find('[tr-scrollflip-scrubend]')
-    let startSetting = attr(
-        'top top',
-        scrubStartEl.attr('tr-scrollflip-scrubstart')
-      ),
-      endSetting = attr(
-        'bottom bottom',
-        scrubEndEl.attr('tr-scrollflip-scrubend')
-      ),
-      staggerSpeedSetting = attr(
-        0,
-        componentEl.attr('tr-scrollflip-staggerspeed')
-      ),
-      staggerDirectionSetting = attr(
-        'start',
-        componentEl.attr('tr-scrollflip-staggerdirection')
-      ),
-      scaleSetting = attr(false, componentEl.attr('tr-scrollflip-scale')),
-      breakpointSetting = attr(0, componentEl.attr('tr-scrollflip-breakpoint'))
-    let componentIndex = index,
-      timeline,
-      resizeTimer
-    // asign matching data flip ids
-    originEl.each(function (index) {
-      let flipId = `${componentIndex}-${index}`
-      $(this).attr('data-flip-id', flipId)
-      targetEl.eq(index).attr('data-flip-id', flipId)
-    })
-    // create timeline
-    function createTimeline() {
-      if (timeline) {
-        timeline.kill()
-        gsap.set(targetEl, { clearProps: 'all' })
-      }
-      $('body').addClass('scrollflip-relative')
-      gsap.matchMedia().add(`(min-width: ${breakpointSetting}px)`, () => {
-        const state = Flip.getState(originEl)
-        timeline = gsap.timeline({
-          scrollTrigger: {
-            trigger: scrubStartEl,
-            endTrigger: scrubEndEl,
-            start: startSetting,
-            end: endSetting,
-            scrub: true,
-          },
-        })
-        timeline.add(
-          Flip.from(state, {
-            targets: targetEl,
-            scale: scaleSetting,
-            stagger: {
-              amount: staggerSpeedSetting,
-              from: staggerDirectionSetting,
+  if (window.innerWidth > 991) {
+    ScrollTrigger.normalizeScroll(true)
+    // scrollflip component
+    $("[tr-scrollflip-element='component']").each(function (index) {
+      let componentEl = $(this),
+        originEl = componentEl.find("[tr-scrollflip-element='origin']"),
+        targetEl = componentEl.find("[tr-scrollflip-element='target']"),
+        scrubStartEl = componentEl.find('[tr-scrollflip-scrubstart]'),
+        scrubEndEl = componentEl.find('[tr-scrollflip-scrubend]')
+      let startSetting = attr(
+          'top top',
+          scrubStartEl.attr('tr-scrollflip-scrubstart')
+        ),
+        endSetting = attr(
+          'bottom bottom',
+          scrubEndEl.attr('tr-scrollflip-scrubend')
+        ),
+        staggerSpeedSetting = attr(
+          0,
+          componentEl.attr('tr-scrollflip-staggerspeed')
+        ),
+        staggerDirectionSetting = attr(
+          'start',
+          componentEl.attr('tr-scrollflip-staggerdirection')
+        ),
+        scaleSetting = attr(false, componentEl.attr('tr-scrollflip-scale')),
+        breakpointSetting = attr(
+          0,
+          componentEl.attr('tr-scrollflip-breakpoint')
+        )
+      let componentIndex = index,
+        timeline,
+        resizeTimer
+      // asign matching data flip ids
+      originEl.each(function (index) {
+        let flipId = `${componentIndex}-${index}`
+        $(this).attr('data-flip-id', flipId)
+        targetEl.eq(index).attr('data-flip-id', flipId)
+      })
+      // create timeline
+      function createTimeline() {
+        if (timeline) {
+          timeline.kill()
+          gsap.set(targetEl, { clearProps: 'all' })
+        }
+        $('body').addClass('scrollflip-relative')
+        gsap.matchMedia().add(`(min-width: ${breakpointSetting}px)`, () => {
+          const state = Flip.getState(originEl)
+          timeline = gsap.timeline({
+            scrollTrigger: {
+              trigger: scrubStartEl,
+              endTrigger: scrubEndEl,
+              start: startSetting,
+              end: endSetting,
+              scrub: true,
             },
           })
-        )
+          timeline.add(
+            Flip.from(state, {
+              targets: targetEl,
+              scale: scaleSetting,
+              stagger: {
+                amount: staggerSpeedSetting,
+                from: staggerDirectionSetting,
+              },
+            })
+          )
+        })
+        $('body').removeClass('scrollflip-relative')
+      }
+      createTimeline()
+      // update on window resize
+      window.addEventListener('resize', function () {
+        clearTimeout(resizeTimer)
+        resizeTimer = setTimeout(function () {
+          createTimeline()
+        }, 250)
       })
-      $('body').removeClass('scrollflip-relative')
-    }
-    createTimeline()
-    // update on window resize
-    window.addEventListener('resize', function () {
-      clearTimeout(resizeTimer)
-      resizeTimer = setTimeout(function () {
-        createTimeline()
-      }, 250)
     })
-  })
+  }
 }
 
 export default flipCode
